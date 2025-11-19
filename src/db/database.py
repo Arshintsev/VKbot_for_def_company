3712d7c09d76ecba4231dc4bac0746bda85a4dc7
Andrey Arshintsev <arshintsev@mail.ru>
2025-11-19 17:42:53 +0300

    fix DATABASE_URL

--- src/db/database.py
+++ src/db/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()  # загружаем .env
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///vk1_bot.db")
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///vk_bot.db")
 SQL_ECHO = os.getenv("SQL_ECHO", "False").lower() == "true"
 
 engine = create_engine(DATABASE_URL, echo=SQL_ECHO, future=True)
