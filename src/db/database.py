fa0d26995b797c7276e6f5b8bd535508499c4e15
Andrey Arshintsev <arshintsev@mail.ru>
2025-11-19 11:58:56 +0300

    Add models.py and create engine

---
+++ src/db/database.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()  # загружаем .env
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///vk1_bot.db")
+SQL_ECHO = os.getenv("SQL_ECHO", "False").lower() == "true"
+
+engine = create_engine(DATABASE_URL, echo=SQL_ECHO, future=True)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
---
+++ src/db/models.py
@@ -0,0 +1,65 @@
+import json
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    DECIMAL,
+    Text,
+    DateTime,
+    func,
+)
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+
+
+class Category(Base):
+    """Модель категории."""
+
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    products = relationship(
+        "Product", back_populates="category", cascade="all, delete-orphan"
+    )
+
+
+class Product(Base):
+    """Модель товара."""
+
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    image_url = Column(String)
+    price = Column(DECIMAL(8, 2))
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="products")
+
+
+class UserSession(Base):
+    """Модель для хранения состояний"""
+
+    __tablename__ = "user_sessions"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False, unique=True)  # VK ID пользователя
+    state = Column(String, nullable=False, default="START")  # Текущее состояние машины
+    context = Column(Text, default="{}")  # Дополнительные данные (JSON)
+    last_interaction = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    def get_context(self) -> dict:
+        """Возвращает context как словарь."""
+        try:
+            return json.loads(self.context)
+        except json.JSONDecodeError:
+            return {}
+
+    def set_context(self, data: dict):
+        """Сохраняет context из словаря."""
+        self.context = json.dumps(data)
